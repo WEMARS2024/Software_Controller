a0815c8e9de9baba34dc1c217e18bc2377fabbbd
devpatel2003 <devpatel2003@hotmail.com>
2023-11-07 01:06:32 -0500

    1.1 (Not tested, need to add error responses)

---
+++ Rover/ServerUDP.py
@@ -0,0 +1,17 @@
+import socket
+
+# Set up the UDP server
+rpi_ip = ""   # The IP address where the server listens
+rpi_port = 2222       # The port on which the server listens
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((rpi_ip, rpi_port))
+
+print("UDP server listening on {}:{}".format(rpi_ip, rpi_port))
+
+
+while True:
+    data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
+    print("Received message:", data.decode(), "from", addr)
+
